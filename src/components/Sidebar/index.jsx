3f56dd5df54cff5567ca0c4ea605a8693cdd8a1e
Gui-dev <adrianon10@gmail.com>
2020-01-24 02:51:19 -0300

    Configurando as medias queries do css, e adicionando os estados do Sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,34 +1,79 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import './Sidebar.css'
 
 function Sidebar() {
 
+  const [ github_username, setGituhubUsername ] = useState( '' )
+  const [ techs, setTechs ] = useState( '' )
+  const [ latitude, setLatitude ] = useState( '' )
+  const [ longitude, setLongitude ] = useState( '' )
+
+  useEffect( () => {
+    navigator.geolocation.getCurrentPosition( 
+      position => {
+        const { latitude, longitude } = position.coords
+        setLatitude( latitude )
+        setLongitude( longitude )
+      },
+      error => {
+        console.log( error )
+      }, {
+        timeout: 30000
+      } 
+    )
+  }, [] ) 
+
+  const handleAddDev = ( e ) => {
+    e.preventDefault()
+    
+  }
+
   return (
     <aside className="form-register">
       <h1 className="form-register_title">Cadastrar</h1>
 
-      <form className="form-register_form">
+      <form className="form-register_form" onSubmit={ handleAddDev }>
         <div className="input-block">
           <label htmlFor="github_username" className="form-register_label">Usuário do Github</label>
-          <input type="text" name="github_username" id="github_username" className="form-register_input" required/>
+          <input type="text" name="github_username" id="github_username" 
+            className="form-register_input" 
+            required
+            value={ github_username }
+            onChange={ e => setGituhubUsername( e.target.value ) }
+          />
         </div>
 
         <div className="input-block">
           <label htmlFor="techs" className="form-register_label">Tecnologias</label>
-          <input type="text" name="techs" id="techs" className="form-register_input" required/>
+          <input type="text" name="techs" id="techs" 
+            className="form-register_input" 
+            required
+            value={ techs }
+            onChange={ e => setTechs( e.target.value ) }
+          />
         </div>
 
         <div className="input-group">
           
           <div className="input-block">
             <label htmlFor="latitude" className="form-register_label">Latitude</label>
-            <input type="text" name="latitude" id="latitude" className="form-register_input" required/>
+            <input type="number" name="latitude" id="latitude" 
+              className="form-register_input" 
+              required
+              value={ latitude }
+              onChange={ e => setLatitude( e.target.value ) }
+            />
           </div>
             
           <div className="input-block">
             <label htmlFor="longitude" className="form-register_label">Longitude</label>
-            <input type="text" name="longitude" id="longitude" className="form-register_input" required/>
+            <input type="number" name="longitude" id="longitude" 
+              className="form-register_input" 
+              required
+              value={ longitude }
+              onChange={ e => setLongitude( e.target.value ) }
+            />
           </div>
 
         </div>
